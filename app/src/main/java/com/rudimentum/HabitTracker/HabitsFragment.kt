c920db3cfedb666b95ba4415d249f22c209e5810
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-07-01 18:56:03 +0300

    Create Fragment for list of habits

---
+++ app/src/main/java/com/rudimentum/HabitTracker/HabitsFragment.kt
@@ -0,0 +1,38 @@
+package com.rudimentum.HabitTracker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_habits.*
+
+
+class HabitsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_habits, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = HabitsFragment()
+    }
+}
