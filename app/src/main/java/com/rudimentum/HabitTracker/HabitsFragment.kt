29c8164b831dd664382a49c7a2e984a6e33c2121
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-07-04 22:39:01 +0300

    Implement RecyclerView inside Fragment

--- app/src/main/java/com/rudimentum/HabitTracker/HabitsFragment.kt
+++ app/src/main/java/com/rudimentum/HabitTracker/HabitsFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
+import com.rudimentum.HabitTracker.adapter.HabitsListAdapter
 import kotlinx.android.synthetic.main.fragment_habits.*
 
 
 class HabitsFragment : Fragment() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<HabitsListAdapter.MyViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    private val habits = listOf("Running", "Swimming", "Reading")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +33,14 @@ class HabitsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        habitsListRecyclerView.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = HabitsListAdapter(habits)
+        }
+        // the code just for checking of button
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
--- app/src/main/java/com/rudimentum/HabitTracker/MainActivity.kt
+++ app/src/main/java/com/rudimentum/HabitTracker/MainActivity.kt
@@ -2,6 +2,9 @@ package com.rudimentum.HabitTracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rudimentum.HabitTracker.adapter.HabitsListAdapter
+import kotlinx.android.synthetic.main.fragment_habits.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
